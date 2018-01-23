f5296c7709cd06366b2c4a88207d5a3365712e0a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-23 08:34:26 +0530

    More dependencies added

--- app/src/main/java/rm/iamjosephvarghese/deadlock2k18_question_upload/Upload.java
+++ app/src/main/java/rm/iamjosephvarghese/deadlock2k18_question_upload/Upload.java
@@ -1,11 +1,23 @@
 package rm.iamjosephvarghese.deadlock2k18_question_upload;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
 
 public class Upload extends AppCompatActivity {
+
+
+    private int STORAGE_PERMISSION_CODE = 23;
+    private int CAMERA_PERMISSION_CODE = 24;
+
+
+    private StorageReference mStorageRef;
+    private StorageMetadata metadata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +25,13 @@ public class Upload extends AppCompatActivity {
         setContentView(R.layout.activity_upload);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        StrictMode.VmPolicy.Builder builderStrict = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builderStrict.build());
+
+
 
 
 
